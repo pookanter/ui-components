7f89faa33c474fc1a66fa182dd4366106da8514f
sippakorn-phuakpong <cloneltorrer@gmail.com>
2024-12-06 01:19:01 +0700

    [Popper]: test

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,33 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React from "react";
 import "./App.css";
+import * as Popper from "./components/popper";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const [open, setOpen] = React.useState(false);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Popper.Root>
+        <Popper.Anchor>
+          {open}
+          <button onClick={() => setOpen(!open)}>OPEN POPPER</button>
+        </Popper.Anchor>
+        {open && (
+          <Popper.Content>
+            <div
+              style={{
+                background: "#333",
+                color: "#fff",
+                padding: "10px",
+                borderRadius: "4px",
+                position: "relative",
+                display: "inline-block",
+              }}>
+              <button onClick={() => setOpen(false)}>close</button>
+              <p style={{ margin: 0 }}>Popper is a tool tip</p>
+            </div>
+          </Popper.Content>
+        )}
+      </Popper.Root>
     </>
   );
 }
