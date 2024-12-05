f9a072a19646634535ba575eba72ce17cb8a5185
sippakorn-phuakpong <cloneltorrer@gmail.com>
2024-12-05 20:30:36 +0700

    update alias path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+//@ts-expect-error TS2307
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      //@ts-expect-error TS2307
+      "@components": path.resolve(__dirname, "./src/components"),
+    },
+  },
+});
